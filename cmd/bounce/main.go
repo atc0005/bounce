f87a3f65071129f25efca9ff4fb5e15db99946c1
Adam Chalkley <atc0005@users.noreply.github.com>
2020-03-04 11:48:10 -0600

    WIP: Untested implementation of flags for log format, output and level

--- cmd/bounce/main.go
+++ cmd/bounce/main.go
@@ -22,59 +22,25 @@ import (
 
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/cli"
+	"github.com/apex/log/handlers/discard"
+	"github.com/apex/log/handlers/json"
 	"github.com/apex/log/handlers/logfmt"
-	"github.com/apex/log/handlers/multi"
+	"github.com/apex/log/handlers/text"
 )
+
+// for handler in cli discard es graylog json kinesis level logfmt memory
+// multi papertrail text delta; do go get
+// github.com/apex/log/handlers/${handler}; done
 
 // see templates.go for the hard-coded HTML/CSS template used for the index
 // page
 
 func main() {
 
-	// 	apex/log Handlers
-	// ---------------------------------------------------------
-	// cli - human-friendly CLI output
-	// discard - discards all logs
-	// es - Elasticsearch handler
-	// graylog - Graylog handler
-	// json - JSON output handler
-	// kinesis - AWS Kinesis handler
-	// level - level filter handler
-	// logfmt - logfmt plain-text formatter
-	// memory - in-memory handler for tests
-	// multi - fan-out to multiple handlers
-	// papertrail - Papertrail handler
-	// text - human-friendly colored output
-	// delta - outputs the delta between log calls and spinner
-
-	// create error logger that directs messages to stderr
-	// create stdout logger specifically for providing status updates (pretty)
-	// create logfmt stdout logger for systemd consumption if `-daemon` flag is used
-
-	// seems apex/log is different. We don't seem to be able to create
-	// multiple logger objects, each to a different output target?
-	log.SetHandler(multi.New(
-		logfmt.New(os.Stderr),
-		cli.New(os.Stdout),
-	))
-	log.SetLevel(log.DebugLevel)
-	// log.SetHandler(logfmt.New(os.Stderr))
-
-	//log.NewEntry(logfmt.New(os.Stdout))
-
-	// stderrLogger := &log.Logger{}
-
-	// stderrLogger.SetHandler()
-
-	// ctx := log.WithFields(log.Fields{
-	// 	"file": "something.png",
-	// 	"type": "image/png",
-	// 	"user": "tobi",
-	// })
-
-	// ctx.Info("Hello")
-
-	log.Debug("Initializing application")
+	// This will use default logging settings (level filter, destination)
+	// as the application hasn't "booted up" far enough to apply custom
+	// choices yet.
+	log.Info("Initializing application")
 
 	appConfig, err := config.NewConfig()
 	if err != nil {
@@ -84,7 +50,41 @@ func main() {
 		log.Fatalf("Failed to initialize application: %s", err)
 	}
 
-	log.Debugf("AppConfig: %+v\n", appConfig)
+	var logOutput *os.File
+	switch appConfig.LogOutput {
+	case config.LogOutputStderr:
+		logOutput = os.Stderr
+	case config.LogOutputStdout:
+		logOutput = os.Stdout
+	}
+
+	switch appConfig.LogFormat {
+	case config.LogFormatCLI:
+		log.SetHandler(cli.New(logOutput))
+	case config.LogFormatJSON:
+		log.SetHandler(json.New(logOutput))
+	case config.LogFormatLogFmt:
+		log.SetHandler(logfmt.New(logOutput))
+	case config.LogFormatText:
+		log.SetHandler(text.New(logOutput))
+	case config.LogFormatDiscard:
+		log.SetHandler(discard.New())
+	}
+
+	switch appConfig.LogLevel {
+	case config.LogLevelFatal:
+		log.SetLevel(log.FatalLevel)
+	case config.LogLevelError:
+		log.SetLevel(log.ErrorLevel)
+	case config.LogLevelWarn:
+		log.SetLevel(log.WarnLevel)
+	case config.LogLevelInfo:
+		log.SetLevel(log.InfoLevel)
+	case config.LogLevelDebug:
+		log.SetLevel(log.DebugLevel)
+	}
+
+	log.Debugf("AppConfig: %+v", appConfig)
 
 	// SETUP ROUTES
 	// See handlers.go for handler definitions
